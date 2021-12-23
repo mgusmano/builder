99a2f92d8e80091c7e43a4ad186fb38d3769a3d7
n0wfal <nowfaln@celestialsys.com>
2021-12-23 12:19:23 +0530

    fix(view generation): fixed a bug caused by switching from sencha cmd to ext-gen
    fixed a bug that caused by view dir detection issue from switching to ext-gen
    
    no breaking changes

--- src/scaffold/Templates.ts
+++ src/scaffold/Templates.ts
@@ -6,13 +6,13 @@ export const mainFile = (n: string) => `Ext.define('myApp.view.${n}', {
 export const modelFile = (n: string) => `/**
  * This class is the view model for the Main view of the application.
  */
-Ext.define('rew.view.main.${n}ViewModel', {
+Ext.define('${n}.view.main.${n}ViewModel', {
     extend: 'Ext.app.ViewModel',
 
     alias: 'viewmodel.main',
 
     data: {
-        name: 'rew',
+        name: '${n}',
 
         loremIpsum: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
@@ -25,7 +25,7 @@ export const controllerFile = (n: string) => `/**
  * This class is the controller for the main view for the application. It is specified as
  * the "controller" of the Main view class.
  */
-Ext.define('rew.view.main.${n}ViewController', {
+Ext.define('${n}.view.main.${n}ViewController', {
     extend: 'Ext.app.ViewController',
 
     alias: 'controller.main',
